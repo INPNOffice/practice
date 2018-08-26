1e89e8b15d0006cfca64a25923445fdf27f20b6d
INPN_Office <yuki-2000@mineo.jp>
2018-08-26 20:18:30 +0900

    correct filename

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ var span = document.getElementById("span");
 span.addEventListener("click", function(e){
     span.innerHTML = "hugahuga";
     span.style.backgroundColor = "#00ffff";
-    console.log("hugahuga");
+    console.log("I love naari.");
 });
