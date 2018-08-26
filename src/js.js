1e4ed5046bd100b1443d035021c193c09466fbb3
INPN_Office <yuki-2000@mineo.jp>
2018-08-26 19:03:27 +0900

    Initial commit

---
+++ src/js.js
@@ -0,0 +1,6 @@
+var span = document.getElementById("span");
+span.addEventListener("click", function(e){
+    span.innerHTML = "hugahuga";
+    span.style.backgroundColor = "#00ffff";
+    console.log("hugahuga");
+});
