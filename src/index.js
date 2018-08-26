4dd72f45f5cffe4c6c7c760258687696d50c00b7
INPN_Office <yuki-2000@mineo.jp>
2018-08-26 22:36:46 +0900

    use domready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-var span = document.getElementById("span");
-span.addEventListener("click", function(e){
-    span.innerHTML = "hugahuga";
-    span.style.backgroundColor = "#00ffff";
-    console.log("I love naari.");
+const domready = require("domready");
+
+domready(function(){
+    var span = document.getElementById("span");
+    span.addEventListener("click", function(e){
+        span.innerHTML = "hugahuga";
+        span.style.backgroundColor = "#00ffff";
+        console.log("I love naari.");
+    });
 });
